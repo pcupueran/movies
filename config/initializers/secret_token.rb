ce54313dd6afdbd4d592b91eb3c617ca0f10ac91
pcupueran <pa1809ty@gmail.com>
2016-08-01 23:59:35 +0100

    Creating rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MoviesApp::Application.config.secret_token = 'b4dc2bcb55bb64470aa1e3576ea4096140499be0f1b284d6e6587134fcd5a475823f59adb77ccc7398a0e4491a53307b8137a1b5b6af6ad6814be0813a4a6a80'
